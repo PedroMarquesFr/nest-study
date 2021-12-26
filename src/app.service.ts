dbf3a678a7c3014ccc99d5d5c5c213945ea8e3f2
Pedro Marques <pedromarques1011@gmail.com>
2021-12-26 16:32:42 -0300

    added route person

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,8 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  getPerson(person: string, age: number): Person {
+    return { person, age };
+  }
 }
---
+++ src/person.dto.ts
@@ -0,0 +1,4 @@
+interface Person {
+  person: string;
+  age: number;
+}
